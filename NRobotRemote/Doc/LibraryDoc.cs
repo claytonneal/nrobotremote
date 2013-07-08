29e23ea139a01e6ce6c4d2e009856ca518dcb9b2
clayton_neal <clayton_neal@yahoo.co.uk>
2013-07-08 15:31:49 +0000

    Issue 5 fixed

--- NRobotRemote/Doc/LibraryDoc.cs
+++ NRobotRemote/Doc/LibraryDoc.cs
@@ -4,6 +4,8 @@ using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 using log4net;
+using System.Text;
+using System.Linq;
 
 namespace NRobotRemote.Doc
 {
@@ -16,15 +18,17 @@ namespace NRobotRemote.Doc
 		//log4net
 		private static readonly ILog log = LogManager.GetLogger(typeof(LibraryDoc));
 		
-		private String _docfile;
-		private XDocument _docxml;
+		private RemoteService _service;
+		private XDocument _docxml = null;
 		
-		public LibraryDoc(String docfile)
+		public LibraryDoc(RemoteService service)
 		{
-			if (String.IsNullOrEmpty(docfile)) throw new ArgumentNullException("Documentation file is null");
-			if (!File.Exists(docfile)) throw new FileNotFoundException("Documentation file not found");
-			_docfile = docfile;
-			_docxml = XDocument.Load(docfile);
+			if (service==null) throw new Exception("No Service specified for LibraryDoc");
+			_service = service;
+			//check
+			if (String.IsNullOrEmpty(_service._config.docfile)) throw new ArgumentNullException("Documentation file is null");
+			if (!File.Exists(_service._config.docfile)) throw new FileNotFoundException("Documentation file not found");
+			_docxml = XDocument.Load(_service._config.docfile);
 			log.Debug("XML Documentation file loaded");
 		}
 		
@@ -33,10 +37,42 @@ namespace NRobotRemote.Doc
 		/// </summary>
 		public String GetMethodDoc(MethodInfo method)
 		{
-			var doc = method.GetXmlDocumentation(_docxml);
-			log.Debug(String.Format("Documentation for method {0} : {1}",method.Name,doc));
-			return doc;
+			if (_docxml!=null)
+			{
+				var doc = method.GetXmlDocumentation(_docxml);
+				log.Debug(String.Format("Documentation for method {0} : {1}",method.Name,doc));
+				return doc;
+			}
+			else
+			{
+				log.Warn("No xml documentation file loaded, returning empty string");
+				return String.Empty;
+			}
 		}
+		
+		/// <summary>
+		/// Gets a Html table for display of all keyword documentation
+		/// </summary>
+		public String GetHTMLDoc()
+		{
+			//setup
+			StringBuilder html = new StringBuilder();
+			html.Append("<table style=\"text-align: left; width: 90%;\" border=\"1\" cellpadding=\"1\" cellspacing=\"0\">");
+			html.Append("<thead><tr style=\" background-color: rgb(153, 153, 153)\">");
+			html.Append("<th>Keyword</th><th>Arguments</th><th>Description</th></tr>");
+			html.Append("</thead><tbody>");
+			var names = _service._keywordmap.GetKeywordNames();
+			Array.Sort(names);
+			foreach(String name in names)
+			{
+				html.Append(String.Format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>",name,
+				                          String.Join(",",_service._keywordmap.GetKeyword(name).ArgumentNames),
+				                          GetMethodDoc(_service._keywordmap.GetKeyword(name).Method)));
+			}
+			html.Append("</tbody></table>");
+			return html.ToString();
+		}
+		
 		
 	}
 }
