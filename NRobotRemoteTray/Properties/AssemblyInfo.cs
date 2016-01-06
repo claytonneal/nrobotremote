8c8df07c2e15359069e426474a18d3f71fd75b6d
Clayton <cneal@buy4now.ie>
2016-01-06 11:12:16 +0000

    Update assembly version and nuget version

--- NRobotRemoteTray/Properties/AssemblyInfo.cs
+++ NRobotRemoteTray/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyVersion("2.0.1.*")]
 
 //log4net
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
